6c85f4fd69fed66705291eced85ff51106a104d4
Ismo <ismailaidar@gmail.com>
2022-08-04 17:08:24 -0400

    add rating attribute to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
+
+  def rating
+    Rating.where(user: self).average(:stars).to_f
+  end
 end
