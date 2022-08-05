6a9af5647097933fd9e8059f450c04c17552ca82
Ismo <ismailaidar@gmail.com>
2022-08-04 20:02:16 -0400

    Add pagination to posts

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem "puma", "~> 5.0"
 
 gem 'jwt'
 
+gem 'will_paginate', '~> 3.3'
+
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PostsController < ApplicationController
 
   # GET /posts
   def index
-    @posts = Post.all
+    @posts = Post.paginate(:page => params[:page])
 
     render json: ::PostsRepresenter.new(@posts).as_json
   end
