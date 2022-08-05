4fc0915e7cb82194ccfc17c341ca6e20da0dd966
Ismo <ismailaidar@gmail.com>
2022-08-05 07:25:48 -0400

    handle record_not_found & record_not_destroyed exception errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
   include JsonWebToken
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :record_not_destroyed
 
   before_action :authenticate_request
 
@@ -9,5 +11,13 @@ class ApplicationController < ActionController::API
     header = header.split(" ").last if header
     decoded = jwt_decode(header)
     @current_user = User.find(decoded[:user_id])
+  end
+
+  def record_not_found
+    render json: { message: "Record not found" }, status: :not_found
+  end
+
+  def record_not_destroyed
+    render json: { message: "Record not destroyed" }, status: :unprocessable_entity
   end
 end
